e85190af9648a2a1437b0a7ac9d2291ec57b4706
Warissara Wichiansrang <warissara.0039@gmail.com>
2020-09-22 21:12:53 +0700

    available to click image on homepage

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
-new Vue({
-    el: '#app',
-    template: '#search',
-    data() {
-      return {
-        query: ''
-      };
-    }
-  });
+$(function() {
 
-  
+  document.addEventListener('init', function(event) {
+      var page = event.target;
+      
+      console.log(page.id);
+
+      if (page.id === 'Home') {
+          $('#back').hide();
+          page.querySelector('#starwar').onclick = function() {
+              document.querySelector('#myNavigator').pushPage('views/starwar.html');
+            
+          };
+          page.querySelector('#pirate').onclick = function() {
+              document.querySelector('#myNavigator').pushPage('views/pirate.html');
+          };
+          page.querySelector('#avatar').onclick = function() {
+              document.querySelector('#myNavigator').pushPage('views/avatar.html');
+          };
+      } else if (page.id === 'starwar' || page.id === 'pirate' || page.id === 'avatar') {
+          
+          $('#back').show();
+          document.querySelector('ons-back-button').onclick = function(event) {
+              document.querySelector('#myNavigator').popPage();
+              $('#back').hide();
+          };
+      }
+      
+        
+      
+
+  });
+})
